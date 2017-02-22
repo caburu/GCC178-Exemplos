907ff61856eae98a55b687fe26b52f332faacfde
Julio Cesar Alves <julio.caburu@gmail.com>
2017-02-22 14:47:32 -0300

    Inclusão do projeto "ConcessionariaVeiculos" aula Padroes de Projeto Parte 1 (alteracao nome da pasta - antes era FabricaAutomoveis).

---
+++ ConcessionariaVeiculos/src/fabricaautomoveis/carros/Gol.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabricaautomoveis.carros;
+
+/**
+ *
+ * @author julio
+ */
+public class Gol extends Carro {
+    
+    public Gol(String cor) {
+        super("Gol", Marca.VW, cor, 30000, Categoria.POPULAR);
+    }
+    
+}
---
+++ ConcessionariaVeiculos/src/fabricaautomoveis/carros/Uno.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabricaautomoveis.carros;
+
+/**
+ *
+ * @author julio
+ */
+public class Uno extends Carro {
+    
+    public Uno(String cor) {
+        super("Uno", Marca.FIAT, cor, 20000, Categoria.POPULAR);
+    }   
+}
